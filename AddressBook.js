2f062ae97dd6f09d30023e7f10d6bb399342a8e7
masterujjval <guptarickey3@gmail.com>
2025-03-04 01:14:18 +0530

    [Ujjwal Gupta] ADD: UC 2 validation in address book done

--- AddressBook.js
+++ AddressBook.js
@@ -1,14 +1,48 @@
 // Creating a class to store contact info
 class AddressBook {
     // Defining properties
-    constructor(firstName, lastName, city, state, phone, email) {
+    
+
+    
+    constructor(firstName, lastName, address,city, state, phone, email) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.address=address;
         this.city = city;
         this.state = state;
         this.phone = phone;
         this.email = email;
+        this.validate();
     }
+    // inside a class we dont write function while defining a method
+    
+         validate(){
+    let nameReg=new RegExp('^[A-Z]{1}[a-z]{2,}$');
+    let addReg=new RegExp('^[\\w\\s]{3,}$');
+    let phoneReg=new RegExp('^[6-9]{1}[0-9]{9}');
+    let emailReg = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$');
+
+    if(!nameReg.test(this.firstName)|| !nameReg.test(this.lastName)){
+    throw new Error("First and Last name should start with capital letters");
+    }
+    // using else if ladder to check for validation
+    else if(!addReg.test(this.city) || !addReg.test(this.address) || !addReg.test(this.state)){
+    throw new Error("State, City or Address should contain minimum three characters!");
+    }
+     else if(!phoneReg.test(this.phone)){
+    throw new Error("Enter valid phone number!");
+    }
+    else if(!emailReg.test(this.email)){
+        throw new Error("Enter valid email!");
+    
+    }
+    
+    
+    }
+    
+    
+    
+    
 
     // toString method to return string representation
     toString() {
@@ -18,8 +52,12 @@ class AddressBook {
 
 // Main function to test the class
 function main() {
-    let obj = new AddressBook("Ujjwal", "Gupta", "Bhopal", "MP", 9283829483, "ujjug@gmail.com");
+try{
+    let obj = new AddressBook("Ujjwal", "Gupta", "shiv nagar","bhopal", "madhya pradesh", 9283829483, "ujjug@gmail.com");
     console.log(obj.toString()); // Calling toString and printing
+}catch(error){
+console.log(error.message);
+}
 }
 
 // Calling main function
